799c2e6a11a33517c651abadb04ddfa9fdb49256
Sanket Patel <c.sanket.patel@uptake.com>
2017-06-01 18:00:36 -0500

    feat(html): use directive to render custom HTML
    The value of the directive is transcluded inside the element where the directive is placed

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        link: 'http://www.uptake.com'
+        link: 'http://www.uptake.com',
+        customHTML: '<a href="http://www.uptake.com" target="_blank">Uptake!</a>'
     },
     methods: {
         changeTitle: function(event) {
